ec2f5ed52a971b8720286e548a0b95572094cd64
Astra-chan-desu <lyosha5438@gmail.com>
2026-05-26 20:46:16 +0300

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+unless School.exists?
+  school = School.create!(name: "Школа №1")
+
+  class_a = school.school_classes.create!(number: 1, letter: "А")
+  class_b = school.school_classes.create!(number: 1, letter: "Б")
+
+  school.students.create!(
+    first_name: "Вячеслав",
+    last_name: "Абдурахмангаджиевич",
+    surname: "Мухобойников-Сыркин",
+    school_class: class_a
+  )
+
+  school.students.create!(
+    first_name: "Иван",
+    last_name: "Иванов",
+    surname: "Иванович",
+    school_class: class_a
+  )
+
+  school.students.create!(
+    first_name: "Петр",
+    last_name: "Петров",
+    surname: "Петрович",
+    school_class: class_b
+  )
+
+  puts "Test data is put into DB"
+else
+  puts "School exists, no test data is put"
+end
